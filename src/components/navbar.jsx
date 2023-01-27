e04b13efa59ef4f3a5b3ef8b80120d7abe8315fe
Noah Tanner <noahtanner@Noahs-MacBook-Pro-3.local>
2023-01-27 13:23:48 -0800

    made dropdown toggle on and off, still need to refine

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,36 +23,35 @@ const Navbar = () => {
   ]
 
   const listItems = links.map((id) =>
-    <li key={id} className='font-medium text-white'>
+    <li key={id} className='font-medium text-white px-4 cursor-pointer hover:scale-105 duration-200'>
       {id.link}
     </li>
   )
 
-  const [visible, setVisible] = useState(true)
-
-
-  function handleClick() {
-    setVisible(!visible);
-  }
+  const [nav, setNav] = useState(false)
 
   return (
     <div id='root'>
       <div className='flex justify-between items-center w-full h-20 bg-zinc-900 text-zinc-100'>
         <div id="logo">
         </div>
-        <div id='burger-menu-btn' className='absolute top-5 right-5'>
-          <button>
-            <FaBars size={30} color='white'/>
-          </button>
+        <ul id="large-nav" className='hidden md:flex md:flex-col justify-center items-center space-x-4 px-4'>
+          {listItems}
+        </ul>
+
+        <div onClick={() => setNav(!nav)} className='md:hidden cursor-pointer z-100 text-zinc-100'>
+          {nav ? <FaTimes size={30}/>: <FaBars size={30}/>}
         </div>
-        <div>
-            <ul id="nav" className='flex flex-col justify-center items-center space-x-4 px-4'>
-              {listItems}
-            </ul>
-        </div>
+
+        {nav && (
+          <ul className='px-4 cursor-pointer font-medium text-zinc-100' onClick={() => setNav(!nav)}>
+            {listItems}
+          </ul>
+        )}
+
       </div>
     </div>
   );
 }
 
-export default Navbar
+export default Navbar;
